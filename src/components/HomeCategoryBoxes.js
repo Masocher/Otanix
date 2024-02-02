46f7cc55e1249e9222f9f10f1405a2575d6e4708
Masocher <Masochergamer@gmail.com>
2024-02-02 16:46:12 +0330

    Create home's category sections

---
+++ src/components/HomeCategoryBoxes.js
@@ -0,0 +1,30 @@
+// assets
+import img1 from "../images/animes/1.jpg"
+
+const HomeCategoryBoxes = () => {
+    return (
+        <div className="category_boxes">
+            <div className="category_box">
+                <img src={img1} />
+                <div className="title_box">انیمه های اکشن</div>
+            </div>
+
+            <div className="category_box">
+                <img src={img1} />
+                <div className="title_box">انیمه های اکشن</div>
+            </div>
+
+            <div className="category_box">
+                <img src={img1} />
+                <div className="title_box">انیمه های اکشن</div>
+            </div>
+
+            <div className="category_box">
+                <img src={img1} />
+                <div className="title_box">انیمه های اکشن</div>
+            </div>
+        </div>
+    )
+}
+
+export default HomeCategoryBoxes
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 // components
 import HomeSlider from "../components/HomeSlider";
 import HomeArticles from "../components/HomeArticles";
+import HomeCategoryBoxes from "../components/HomeCategoryBoxes";
 
 const Home = () => {
   return (
     <div>
       <HomeSlider />
       <HomeArticles />
+      <HomeCategoryBoxes />
     </div>
   );
 };
