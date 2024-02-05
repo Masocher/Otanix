f036e91c0cc2ee479c4cb6cc79ef03c11271beac
Masocher <Masochergamer@gmail.com>
2024-02-05 18:38:33 +0330

    Create home's season's top animes slider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,8 @@ import HomeArticles from "../components/HomeArticles";
 import HomeCategoryBoxes from "../components/HomeCategoryBoxes";
 import HomeTopics from "../components/HomeTopics";
 import HomeDiscussions from "../components/HomeDiscussions";
+import HomeSeasonTopics from "../components/HomeSeasonTopics";
+import HomeWeeklySchedule from "../components/HomeWeeklySchedule";
 
 const Home = () => {
   return (
@@ -13,6 +15,7 @@ const Home = () => {
       <HomeArticles />
       <HomeTopics />
       <HomeDiscussions />
+      <HomeSeasonTopics />
     </div>
   );
 };
