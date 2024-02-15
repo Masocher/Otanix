59210152436cd8a6461c1e1580d44d26f5984eb6
Masocher <Masochergamer@gmail.com>
2024-02-15 19:38:18 +0330

    Create change theme button

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,10 @@ import { Link } from "react-router-dom";
 // tools
 import { useLocation } from "react-router-dom";
 
+// icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+
 const Header = () => {
   const location = useLocation();
 
@@ -17,27 +21,45 @@ const Header = () => {
 
         <div className="header_links">
           <Link to={"/"} style={{ textDecoration: "none" }}>
-            <div className={`h_link ${location.pathname === "/" ? "show" : ""}`}>
+            <div
+              className={`h_link ${location.pathname === "/" ? "show" : ""}`}
+            >
               صفحه اصلی
             </div>
           </Link>
           <Link to={"/animes"} style={{ textDecoration: "none" }}>
-            <div className={`h_link ${location.pathname === "/animes" ? "show" : ""}`}>
+            <div
+              className={`h_link ${
+                location.pathname === "/animes" ? "show" : ""
+              }`}
+            >
               انیمه ها
             </div>
           </Link>
           <Link to={"/discussions"} style={{ textDecoration: "none" }}>
-            <div className={`h_link ${location.pathname === "/discussions" ? "show" : ""}`}>
+            <div
+              className={`h_link ${
+                location.pathname === "/discussions" ? "show" : ""
+              }`}
+            >
               گفتگو ها
             </div>
           </Link>
           <Link to={"/about"} style={{ textDecoration: "none" }}>
-            <div className={`h_link ${location.pathname === "/about" ? "show" : ""}`}>
+            <div
+              className={`h_link ${
+                location.pathname === "/about" ? "show" : ""
+              }`}
+            >
               درباره ما
             </div>
           </Link>
           <Link to={"/contact"} style={{ textDecoration: "none" }}>
-            <div className={`h_link ${location.pathname === "/contact" ? "show" : ""}`}>
+            <div
+              className={`h_link ${
+                location.pathname === "/contact" ? "show" : ""
+              }`}
+            >
               تماس با ما
             </div>
           </Link>
@@ -45,8 +67,34 @@ const Header = () => {
       </div>
 
       <div className="left_side">
-        <Link to={"/sign-up"} className="sign_btn log_btn" style={{ textDecoration: "none" }}>ثبت نام</Link>
-        <Link to={"/sign-in"} className="sign_btn" style={{ textDecoration: "none" }}>ورود</Link>
+        <div className="theme_mode_wrapper">
+          <span>
+            <FontAwesomeIcon icon={faMoon} />
+          </span>
+
+          <div className="theme_mode_btn">
+            <div className="mode_square"></div>
+          </div>
+
+          <span className="sun_icon">
+            <FontAwesomeIcon icon={faSun} />
+          </span>
+        </div>
+
+        <Link
+          to={"/sign-up"}
+          className="sign_btn log_btn"
+          style={{ textDecoration: "none" }}
+        >
+          ثبت نام
+        </Link>
+        <Link
+          to={"/sign-in"}
+          className="sign_btn"
+          style={{ textDecoration: "none" }}
+        >
+          ورود
+        </Link>
       </div>
     </div>
   );
